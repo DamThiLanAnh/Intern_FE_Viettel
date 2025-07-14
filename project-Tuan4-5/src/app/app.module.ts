1f0e020820aaa9bfbcaa99d74247d09c755e07dd
DamThiLanAnh <damthilananh8@hmail.com>
2025-07-14 19:13:04 +0700

    tuan4-5

--- project-Tuan4-5/src/app/app.module.ts
+++ project-Tuan4-5/src/app/app.module.ts
@@ -5,7 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ListComponent } from './list/list.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
+registerLocaleData(en);
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +31,25 @@ import { ListComponent } from './list/list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'list', component: ListComponent }
+    ]),
+    HttpClientModule,
+    BrowserAnimationsModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzTableModule,
+    NzButtonModule,
+    NzModalModule,
+    NzInputModule,
+    NzIconModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- project-Tuan4-5/src/app/list/list.component.ts
+++ project-Tuan4-5/src/app/list/list.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent {
+  users: User[] = [
+    { id: 1, name: 'Nguyễn Văn A', email: 'a@gmail.com' },
+    { id: 2, name: 'Trần Thị B', email: 'b@gmail.com' },
+  ];
 
-  constructor() { }
+  editingUser: User | null = null;
+  isModalVisible = false;
 
-  ngOnInit(): void {
+  openAddModal() {
+    this.editingUser = null;
+    this.isModalVisible = true;
   }
 
+  openEditModal(user: User) {
+    this.editingUser = { ...user };
+    this.isModalVisible = true;
+  }
+
+  handleOk() {
+    if (this.editingUser?.id) {
+      const index = this.users.findIndex(u => u.id === this.editingUser?.id);
+      if (index > -1) this.users[index] = this.editingUser!;
+    } else {
+      const newId = Math.max(...this.users.map(u => u.id)) + 1;
+      this.users.push({ ...this.editingUser!, id: newId });
+    }
+    this.isModalVisible = false;
+  }
+
+  handleCancel() {
+    this.isModalVisible = false;
+  }
+
+  deleteUser(id: number) {
+    this.users = this.users.filter(u => u.id !== id);
+  }
 }
